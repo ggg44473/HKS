ecc74427d86f98b986ec8a67c764baf62251a810
huangkaiyun <huangkaiyun@hotmail.com.tw>
2019-01-17 11:11:52 +0800

    Modify setting page

--- resources/views/user/settings.blade.php
+++ resources/views/user/settings.blade.php
@@ -11,41 +11,45 @@
 @section('content')
 <div class="container">
     <div class="row m-3">
-        <div class="ml-4 mr-4">
-            <form id="avatarForm" name="form" method="POST" action="{{ route('user.update', auth()->user()->id) }}" enctype="multipart/form-data">
-                @csrf
-                {{ method_field('PATCH') }}
-                <label class="label" data-toggle="tooltip" title="Change your avatar">
-                    <img id="avatar" class="avatar" src="{{ $user->avatar? $user->avatar:asset('/img/icon/user/green.svg') }}">    
-                    <input type="file" name="avatar" id="input" class="sr-only" accept="image/*"/>
-                </label>
-            </form>
-        </div>
-        <div class="ml-2 pt-3">
-            <h4>{{ $user->name }}</h4>
-            <p>{{ $user->position.'   '.$user->created_at }}</p>
-        </div>
+        <h4 class="col-md-7">帳號設定</h4>
     </div>
+    <form name="form" method="POST" action="{{ route('user.update', auth()->user()->id) }}" enctype="multipart/form-data">
+        @csrf
+        {{ method_field('PATCH') }}
+        <div class="card">
+            <div class="card-body">
+                <div class="form-group row">
+                    <div class="col-12">
+                        <span class="u-pl-8 u-pr-8">帳號</span>
+                        <span class="u-pl-8 u-pr-8">{{ $user->email }}</span>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <div class="col-12">
+                        <span for="name" class="u-pl-8 u-pr-8">姓名</span>
+                        <input type="text" class="form-control u-pl-8 u-pr-8" id="name" value="{{ $user->name }}">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <div class="align-self-center text-right">
+                    </div>
+                    <div class="align-self-center ">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <div class="align-self-center text-right">
+                        <span for="avatar">頭像</span>
+                    </div>
+                    <div class="col-md-10 align-self-center">
+                        <img id="avatar" class="avatar" src="{{ $user->getAvatar() }}">
+                        <input type="file" class="u-ml-8 align-self-center" id="input" name="avatar" value="{{ $user->name }}"
+                            accept="image/*">
+                    </div>
+                </div>
+                <hr />
+            </div>
+        </div>
+    </form>
 </div>
-<div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-    <div class="modal-content">
-        <div class="modal-header">
-        <h5 class="modal-title" id="modalLabel">Crop the image</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-        </button>
-        </div>
-        <div class="modal-body">
-        <div class="img-container">
-            <img id="image" src="">
-        </div>
-        </div>
-        <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
-        <button type="button" class="btn btn-primary" id="crop">Crop</button>
-        </div>
-    </div>
-    </div>
 </div>
 @endsection
