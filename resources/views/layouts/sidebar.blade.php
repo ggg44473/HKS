39d4a980e943a40f2a3f8a645a07d5988e0e494b
huangkaiyun <huangkaiyun@hotmail.com.tw>
2019-01-20 20:28:57 +0800

    Fix navbar RWD

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -22,3 +22,10 @@
     <a class="nav-link" href="#"><img src="{{ asset('/img/icon/calendar/w.svg') }}" alt=""></a>
     <a class="nav-link" href="#"><img src="{{ asset('/img/icon/like/w.svg') }}" alt=""></a>
 </nav>
+<div class="row u-pl-8 u-pr-8 bg-primary u-pt-8 sidebar-sm ml-0 mr-0">
+    <a class="col text-center text-white ml-auto mr-auto" href="{{ route('user.okr', auth()->user()->id) }}"><img src="{{ asset('/img/icon/home/w.svg') }}" alt=""><p class="mb-0 small">OKR</p></a>
+    <a class="col text-center text-white ml-auto mr-auto" href="{{ route('organization') }}"><img src="{{ asset('/img/icon/okr/w.svg') }}" alt=""><p class="mb-0 small">組織</p></a>
+    <a class="col text-center text-white ml-auto mr-auto" href="#"><img src="{{ asset('/img/icon/project/w.svg') }}" alt=""><p class="mb-0 small">專案</p></a>
+    <a class="col text-center text-white ml-auto mr-auto" href="#"><img src="{{ asset('/img/icon/calendar/w.svg') }}" alt=""><p class="mb-0 small">日曆</p></a>
+    <a class="col text-center text-white ml-auto mr-auto" href="#"><img src="{{ asset('/img/icon/like/w.svg') }}" alt=""><p class="mb-0 small">追蹤</p></a>
+</div>
