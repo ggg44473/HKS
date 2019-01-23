b34a1d2141f624127b1ce52422640e6b9bd7536b
hilton <h20110213@gmail.com>
2019-01-23 20:14:25 +0800

    Fix timestamp

--- database/migrations/2019_01_23_045529_create_project_user_table.php
+++ database/migrations/2019_01_23_045529_create_project_user_table.php
@@ -17,6 +17,7 @@ class CreateProjectUserTable extends Migration
             $table->increments('id');
             $table->integer('project_id')->unsigned();
             $table->integer('user_id')->unsigned();
+            $table->timestamps();
         });
     }
 
