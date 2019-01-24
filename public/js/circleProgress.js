7a41e9278f81f1f4bf3dbfbce6a7d0843f46e70a
huangkaiyun <huangkaiyun@hotmail.com.tw>
2019-01-25 01:06:30 +0800

    Modify company blade

---
+++ public/js/circleProgress.js
@@ -0,0 +1,8 @@
+$('.circle').circleProgress({
+    size: 50,
+    fill: {
+        gradient: ["#69D1C5", "#3BA99C"]
+    },
+    lineCap: 'round',
+    startAngle: 180
+});
---
+++ public/js/organization.js
@@ -0,0 +1,7 @@
+$('#moreObjective').on('click',function(){
+    $('#okr-tab').click();
+});
+
+$('#moreMember').on('click',function(){
+    $('#member-tab').click();
+});
