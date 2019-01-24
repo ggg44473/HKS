ac7ffb5316485bb9dfc2497f438cf08b182871b4
huangkaiyun <huangkaiyun@hotmail.com.tw>
2019-01-24 11:15:24 +0800

    Add assignee input

--- resources/js/components/ObjectTypeComponent.vue
+++ resources/js/components/ObjectTypeComponent.vue
@@ -1,46 +1,48 @@
 <template>
   <div>
     <label class="mb-0">執行人</label>
-    <model-list-select class="form-control" 
-                        :list="memberList"
-                        option-value="name"
-                        option-text="name"
-                        v-model="selectedMember"
-                        placeholder="請選擇執行人"
-                        @searchchange="searchMember">
-    </model-list-select>
+    <model-list-select
+      class="form-control"
+      :list="memberList"
+      option-value="name"
+      option-text="name"
+      v-model="selectedMember"
+      placeholder="請選擇執行人"
+      @searchchange="searchMember"
+    ></model-list-select>
+    <input type="hidden" name="assignee" v-bind:value="selectedMember.id">
   </div>
 </template>
 
 <script>
-  import { ModelListSelect } from 'vue-search-select'
+import { ModelListSelect } from "vue-search-select";
 
-  export default {
-    data () {
-      return {
-        memberList: [],
-        selectedMember: {},
-        searchText: '',
+export default {
+  data() {
+    return {
+      memberList: [],
+      selectedMember: {},
+      searchText: ""
+    };
+  },
+  props: ["api"],
+  created() {
+    this.searchMember();
+  },
+  methods: {
+    searchMember() {
+      if (this.memberList.length === 0) {
+        axios
+          .get(this.api)
+          .then(response => {
+            this.memberList = response.data;
+          })
+          .catch(error => {});
       }
-    },
-    props: ['api'],
-    created () {
-      this.searchMember()
-    },
-    methods: {
-      searchMember () {
-        if (this.memberList.length === 0) {
-          axios.get(this.api)
-            .then(response => {
-              this.memberList = response.data
-            })
-            .catch(error => {
-            })
-        }
-      },
-    },
-    components: {
-      ModelListSelect
     }
+  },
+  components: {
+    ModelListSelect
   }
+};
 </script>
