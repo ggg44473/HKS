8bdb76edbae329ca56fbaa9c08f7995db038f263
hilton <h20110213@gmail.com>
2019-02-02 15:31:28 +0800

    Updated notify & fix action show bug

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Console;
 
 use App\Objective;
+use App\KeyResult;
 use App\Action;
 use App\Activity;
 use App\KeyResultRecord;
@@ -12,9 +13,9 @@ use App\Notifications\CheckNotification;
 use App\Notifications\DeadlineNotification;
 use App\Notifications\ActionNotification;
 use App\Notifications\ActivityNotification;
+use App\Notifications\UpdatedNotification;
 
 use Illuminate\Database\Eloquent\Model;
-
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -37,22 +38,53 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // 每半小時掃蕩以下排程
+        // 每半小時掃蕩以下排程，即將開始的行事曆活動提醒
         $schedule->call(function () {
             $now = now();
             $future = now()->addMinutes(40);
+            $past = now()->subMinutes(40);
+
             $activities = Activity::where('started_at', '<=', $future)->where('started_at', '>=', $now)->get();
             foreach ($activities as $activity) {
-                // 四十分鐘內將進行的，會提醒行事曆上的活動即將開始
-                    Notification::send($activity->user, new ActivityNotification($activity));
+                Notification::send($activity->user, new ActivityNotification($activity));
+            }
+            $acts = KeyResult::where('updated_at', '>=', $past)->where('updated_at', '<', $now)->get();
+            foreach ($acts as $act) {
+                Notification::send($act->follower, new ActivityNotification($activity));
+            }
+            $acts = Action::where('updated_at', '>=', $past)->where('updated_at', '<', $now)->get();
+            foreach ($acts as $act) {
+                Notification::send($act->follower, new ActivityNotification($activity));
             }
         })->everyThirtyMinutes();
+
+        // 每小時掃蕩以下排程，提醒更新過的KR、Action給追蹤者確認更動內容
+        $schedule->call(function () {
+            $now = now();
+            $past = now()->subHour();
+            $krs = KeyResult::where('updated_at', '>=', $past)->get();
+            foreach ($krs as $kr) {
+                if ($kr->objective->model->follower->count() > 0) {
+                    foreach ($kr->objective->model->follower as $follower) {
+                        Notification::send($follower->user, new UpdatedNotification($kr));
+                    }
+                }
+            }
+            $acts = Action::where('updated_at', '>=', $past)->get();
+            foreach ($acts as $act) {
+                if ($act->user->follower->count() > 0) {
+                    foreach ($act->user->follower as $follower) {
+                        Notification::send($follower->user, new UpdatedNotification($act));
+                    }
+                }
+            }
+        })->hourly();
 
         // 每日指定時間進行以下排程
         $schedule->call(function () {
             $now = now()->toDateString();
+
             $objs = Objective::where('started_at', '<=', $now)->where('finished_at', '>=', $now)->get();
-            $acts = Action::where('started_at', '<=', $now)->where('finished_at', '>=', $now)->get();
             foreach ($objs as $obj) {
                 // 針對O底下的KR做歷史紀錄
                 foreach ($obj->keyresults as $keyresult) {
@@ -74,6 +106,8 @@ class Kernel extends ConsoleKernel
                     Notification::send($obj->model->getNotifiableUser(), new DeadlineNotification($obj));
                 }
             }
+
+            $acts = Action::where('started_at', '<=', $now)->where('finished_at', '>=', $now)->get();
             foreach ($acts as $act) {
                 // 如果七天後到期，今天會提醒
                 if ($act->finished_at == now()->subWeek()->toDateString()) {
@@ -84,7 +118,7 @@ class Kernel extends ConsoleKernel
                     Notification::send($act->user, new ActionNotification($act));
                 }
             }
-        })->dailyAt('16:00');
+        })->dailyAt('01:00');
     }
 
     /**
