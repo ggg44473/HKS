5ba7af14e7d70faf11b49a699d557e19aa9b9677
huangkaiyun <huangkaiyun@hotmail.com.tw>
2019-02-07 17:46:58 +0800

    Fix tooltip hidden while modal closing

--- public/js/tooltip.js
+++ public/js/tooltip.js
@@ -2,4 +2,8 @@
 $(function () {
     $('[data-toggle="tooltip"]').tooltip();
     $('.tooltipBtn').tooltip();
-})
+});
+
+$('.modal').on('shown.bs.modal', function(e){
+    $('.tooltipBtn').one('focus', function(e){$(this).blur();});
+});
