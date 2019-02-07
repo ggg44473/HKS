314aa352b0c760681943087c8c88d074843f6a90
huangkaiyun <huangkaiyun@hotmail.com.tw>
2019-02-07 10:51:50 +0800

    Add avatar upload while registering

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,6 +13,7 @@
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
     <script src="{{ asset('js/scroll.js') }}" defer></script>
+    @yield('script')
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
