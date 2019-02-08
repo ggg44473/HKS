2125415a6231ce043d68940a12112149793cf913
huangkaiyun <huangkaiyun@hotmail.com.tw>
2019-02-08 15:33:47 +0800

    Fix side bar cookie

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -46,7 +46,7 @@
     <div id="app">
         @include('layouts.navbar')
         @include('layouts.sidebar')
-        <main class="py-4 content u-pb-48 {{ $_COOKIE['openSideBar'] == 'true'? 'open':'' }}">
+        <main class="py-4 content u-pb-48 {{ isset($_COOKIE['openSideBar']) && $_COOKIE['openSideBar'] == 'true'? 'open':'' }}">
             @yield('content')
         </main>
     </div>
